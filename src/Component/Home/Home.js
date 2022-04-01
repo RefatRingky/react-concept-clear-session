ef3169fdefa6e53f52e900b99f8282847dbbcbde
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-02 02:47:49 +0600

    usetshirst function use

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import useTshirt from '../../hooks/useTshirst';
+import Cart from '../Cart/Cart';
+import TShirt from '../TShirt/TShirt';
+import './Home.css'
 
 const Home = () => {
+    const [tShirts,setTshirts] = useTshirt();
     return (
-        <div>
-            <h1>This is home</h1>
+        <div className='home-container'>
+            <div className='tshirt-container'>
+                 {
+                    tShirts.map(tShirt => <TShirt
+                        key={tShirt._id}
+                        tShirt={tShirt}
+                        
+                    ></TShirt>)
+                 }
+            </div>
+            <div className='cart-container'>
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
