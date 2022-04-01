c8eccfccb073d8a0205e2f18817434f93c539ad5
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-02 02:05:21 +0600

    link create commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
-import { Router, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Component/Header/Header';
 import Home from './Component/Home/Home';
 import OrderReview from './Component/OrderReview/OrderReview';
+
 function App() {
  
   return (
     <div className="App">
     <Header></Header>
     <Routes>
-      <Router path='/' elemenet={<Home></Home>} ></Router>
-      <Router path='/home' elemenet={<Home></Home>}></Router>
-      <Router path ='orderReview' elemenet={<OrderReview></OrderReview>}></Router>
+      <Route path='/' element={<Home></Home>}></Route>
+      <Route path='/home' element={<Home></Home>}></Route>
+      <Route path='/orderreview' element={<OrderReview></OrderReview>}></Route>
     </Routes>
     
     </div>
--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+
 const Header = () => {
     return (
         <div>
-            <h1> Welcome To This is header</h1>
+            <h1> Welcome To Tshirst Mania</h1>
             <nav>
-                <Link to="/home">Home</Link>
-                <Link to="/orderReview">Order</Link>
+               
+               <Link to="/home">Home</Link>
+               <Link to="/orderreview">Order Review</Link>
             </nav>
         </div>
     );
