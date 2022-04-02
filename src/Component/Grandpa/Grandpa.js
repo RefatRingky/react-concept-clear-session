fdff928e9a6af5455846e5d57af098528e5f23b8
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-04-02 13:32:20 +0600

    remove item button create

---
+++ src/Component/Grandpa/Grandpa.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Grandpa = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Grandpa;
--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import useTshirt from '../../hooks/useTshirst';
 import Cart from '../Cart/Cart';
+
 import TShirt from '../TShirt/TShirt';
 import './Home.css'
 
 const Home = () => {
     const [tShirts,setTshirts] = useTshirt();
+    const [cart,setCart] = useState([]);
+    const handleAddToCart = (selectedItem)=>{
+        const newCart = [...cart,selectedItem];
+        setCart(newCart);
+    }
+    const handleRemoveFromCart = (selectedItem)=>{
+
+    }
+    
     return (
         <div className='home-container'>
             <div className='tshirt-container'>
@@ -13,12 +23,17 @@ const Home = () => {
                     tShirts.map(tShirt => <TShirt
                         key={tShirt._id}
                         tShirt={tShirt}
+                        handleAddToCart ={handleAddToCart}
                         
                     ></TShirt>)
                  }
             </div>
             <div className='cart-container'>
-                <Cart></Cart>
+
+            <Cart
+            handleRemoveFromCart={handleRemoveFromCart}
+            cart={cart}
+            ></Cart>
             </div>
         </div>
     );
